22b1a2d996324179875c8b6717ad122641cd70c0
Bahram <baxxxrrram@yandex.ru>
2022-09-18 15:43:21 +0400

    add axios create list

--- src/components/supplierslist/List.jsx
+++ src/components/supplierslist/List.jsx
@@ -1,6 +1,62 @@
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../../utils/axios";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
 const List = () => {
+
+    const [list, setList] = useState([]);
+
+    console.log(list)
+
+    useEffect(() =>{
+        axiosInstance.get()
+        .then(response => setList(response.data))
+    }, [list])
+
+    console.log(list) 
     return(
-        <h1>Suppliers</h1>
+        <>
+        <Box sx={{ bgcolor: '#cfe8fc', p: "5rem"}} align="center">            
+            <Typography sx={{color: '#f542ec'}} align="center" variant="h3" gutterBottom>
+                Suppliers
+            </Typography>
+            <Table>
+                <TableHead>
+                    <TableRow>                        
+                        <TableCell sx={{fontSize:"32px"}} align="left">ID</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Company Name</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Contact Title</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Contact Name</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Country</TableCell>
+                        <TableCell sx={{fontSize:"32px"}} align="left">Details</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>                    
+                    {
+                                    
+                        list.map(item => ( 
+                            <TableRow key={item.id}>
+                                <>
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.id}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.companyName}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.contactTitle}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item?.contactName}</TableCell>                            
+                                    <TableCell sx={{fontSize:"24px"}}>{item.address?.country}</TableCell>
+                                    <TableCell sx={{fontSize:"24px"}}>details</TableCell>                
+                                </>                        
+                            </TableRow>))                        
+                    }
+                
+                </TableBody>
+            </Table>
+        </Box>
+        </>
     )
 }
 
